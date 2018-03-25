0ad1b13edf04f06e35cc6fc8017fe6ca7ff20b20
Kamesh19951015 <shanuuuk@gmail.com>
2018-03-25 23:16:25 +0530

    NS fixed entire System.

--- Hypercent.Wings/Hypercent.Wings.Service/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Hypercent.Wings.Service.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
---
+++ MyVehicleTracking.System/MyVehicleTrackingSystem.Wings/Areas/HypercentPortal/HypercentPortalAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace MyVehicleTrackingSystem.Wings.Areas.HypercentPortal
+{
+    public class HypercentPortalAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "HypercentPortal";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "HypercentPortal_default",
+                "HypercentPortal/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "MyVehicleTrackingSystem.Wings.Areas.HypercentPortal.Controllers" }
+            );
+        }
+    }
+}
