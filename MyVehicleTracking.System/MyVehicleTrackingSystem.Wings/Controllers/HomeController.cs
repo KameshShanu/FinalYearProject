10dcf0f1526ebfb4864c34833e7f78f600c452ef
Kamesh Ganegama <shanuuuk@gmail.com>
2018-05-10 23:11:37 +0530

    Added New Tracking Location Map.

--- MyVehicleTracking.System/MyVehicleTrackingSystem.Wings/Controllers/HomeController.cs
+++ MyVehicleTracking.System/MyVehicleTrackingSystem.Wings/Controllers/HomeController.cs
@@ -9,23 +9,10 @@ using System.Web.Mvc;
 namespace MyVehicleTrackingSystem.Wings.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly INewTrackingDeviceService _newTrackingDeviceService;
-        
+    {            
         public ActionResult Index()
         {
             return View();
-        }
-
-        public HomeController(NewTrackingDeviceService newTrackingDeviceService)
-        {
-
-        }
-
-        public JsonResult GetAllLocation()
-        {
-            NewTrackingDevice newTrackingDevice = new NewTrackingDevice();
-            return Json(JsonRequestBehavior.AllowGet);
         }
     }
 }
--- MyVehicleTracking.System/MyVehicleTrackingSystem.Wings/Controllers/InvoiceController.cs
+++ MyVehicleTracking.System/MyVehicleTrackingSystem.Wings/Controllers/InvoiceController.cs
@@ -6,6 +6,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace MyVehicleTrackingSystem.Wings.Controllers
 {
@@ -25,8 +27,33 @@ namespace MyVehicleTrackingSystem.Wings.Controllers
         // GET: Invoice
         public ActionResult Open()
         {
-            IEnumerable<InvoiceViewModel> invoiceModels = new Collection<InvoiceViewModel>();
-            return View(invoiceModels);
+            IEnumerable<NewTrackingDeviceModel> newTrackingDeviceModel = new Collection<NewTrackingDeviceModel>();
+            string markers = "[";
+            string conString = ConfigurationManager.ConnectionStrings["WingsContext"].ConnectionString;
+            SqlCommand cmd = new SqlCommand("SELECT TOP 1 *  FROM NewTrackingDevice ORDER BY [TrackingDeviceId] DESC;");
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                cmd.Connection = con;
+                con.Open();
+                using (SqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    while (sdr.Read())
+                    {
+                        markers += "{";
+                        //markers += string.Format("'title': '{0}',", sdr["Name"]);
+                        markers += string.Format("'lat': '{0}',", sdr["Latitude"]);
+                        markers += string.Format("'lng': '{0}',", sdr["Longitude"]);
+                        markers += string.Format("'Ground_speed': '{0}',", sdr["Ground_speed"]);
+                        markers += string.Format("'Vehicle_angal': '{0}'", sdr["Vehicle_angal"]);
+                        markers += "},";
+                    }
+                }
+                con.Close();
+            }
+
+            markers += "];";
+            ViewBag.Markers = markers;
+            return View();
         }
     }
 }
